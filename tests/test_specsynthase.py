90fcb0cdf98061127028148df33a827ec9e92a07
Ben Hearsum <ben@hearsum.ca>
2017-07-24 08:18:45 -0400

    Add test for .dump()

--- tests/test_specsynthase.py
+++ tests/test_specsynthase.py
@@ -1,5 +1,21 @@
+import os
+from os import path
+import yaml
+
 from specsynthase.specbuilder import SpecBuilder
 
 
 def test_main():
     spec = SpecBuilder()
+
+
+def test_dump():
+    base_dir = path.join(path.abspath(path.dirname(__file__)), "fixtures", "petstore")
+    parts = ["base.yml", "definitions.yml", "paths.yml"]
+    full = yaml.load(open(path.join(base_dir, "full.yml")).read())
+
+    spec = SpecBuilder()
+    for p in parts:
+        spec.add_spec(path.join(base_dir, p))
+
+    assert full == spec
