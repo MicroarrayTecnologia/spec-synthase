cd3de2083bcf06f17e0876037c02e87d3b03144d
Allan Silva <allan.tavares@allantavares.com.br>
2018-07-27 09:44:45 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements(req_file):
 
 setup(
     name='spec-synthase',
-    version='0.1.7',
+    version='0.1.8',
     license='MPL2',
     description='spec-synthase is a tool to help deal with big swagger files, by building the swagger specification files from little spec files. ',
     long_description=read('README.rst'),
